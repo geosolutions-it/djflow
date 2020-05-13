111b2309ebeeaf002c03a2a443058d6bd3bb8e20
bieganowski <bieganowski.rev@gmail.com>
2020-05-13 16:35:46 +0200

    tests 1

--- django_wfe/models.py
+++ django_wfe/models.py
@@ -1,8 +1,16 @@
+import os
+import uuid
 import typing
+import datetime
 import importlib
+import traceback
 
 from django.db import models
 from django.contrib.postgres.fields import JSONField
+
+from .logging import Tee
+from .settings import WFE_LOG_DIR
+from .exceptions import FinishedWorkflow, InputRequired, WrongState
 
 
 class JobState:
@@ -79,6 +87,7 @@ class Job(models.Model):
     A table keeping the serialized state of a certain workflows' executions.
     """
 
+    uuid = models.UUIDField(default=uuid.uuid4)
     workflow = models.ForeignKey(Workflow, on_delete=models.CASCADE)
     current_step = models.ForeignKey(
         Step, on_delete=models.CASCADE, null=True, default=None
@@ -89,6 +98,7 @@ class Job(models.Model):
         default=default_storage,
     )
     state = models.CharField(max_length=20, null=True, default=JobState.PENDING)
+    logfile = models.CharField(max_length=300, default=None)
 
     def save(
         self, force_insert=False, force_update=False, using=None, update_fields=None
@@ -102,10 +112,28 @@ class Job(models.Model):
                     "Step '__start__' not found. Please make sure it is present in the database before ordering a Job."
                 )
 
+        if self.logfile is None:
+            self.logfile = os.path.join(
+                WFE_LOG_DIR, f"{self.workflow.name}_{self.uuid}.log"
+            )
+
         super().save()
 
     def __str__(self):
         return f"{self.workflow.name}:{self.id}"
+
+    @staticmethod
+    def import_class(path: str):
+        """
+        Method importing a certain class from python module.
+
+        :param path: python path (dot notation) to the class
+        :return: class object under located under the provided path
+        """
+        module, class_ = path.rsplit(".", 1)
+        Class = getattr(importlib.import_module(module), class_)
+
+        return Class
 
     def execute(self):
         """
@@ -116,6 +144,7 @@ class Job(models.Model):
         try:
             self._run_next()
         except Exception:
+            self._log("---- WORKFLOW EXECUTION FAILED ----")
             self.state = JobState.FAILED
             self.save()
 
@@ -126,15 +155,15 @@ class Job(models.Model):
         :return:
         :raises: pydantic.ValidationError
         """
-        CurrentStep = self._import_class(self.current_step.path)
+        CurrentStep = self.import_class(self.current_step.path)
 
         if not CurrentStep.UserInputSchema.__fields__:
-            raise RuntimeError(
+            raise WrongState(
                 f"Current Workflow's Step {CurrentStep} does not accept external input."
             )
 
         if self.state != JobState.INPUT_REQUIRED:
-            raise RuntimeError(f"Wrong Workflow's state: {self.state}.")
+            raise WrongState(f"Wrong Workflow's state: {self.state}.")
 
         # pydantic validate the data structure
         external_data = CurrentStep.UserInputSchema(**external_data)
@@ -158,27 +187,93 @@ class Job(models.Model):
 
         :return: None
         """
-        CurrentStep = self._import_class(self.current_step.path)
-        Workflow = self._import_class(self.workflow.path)
+        StepClass = self.import_class(self.current_step.path)
+        WorkflowClass = self.import_class(self.workflow.path)
 
-        current_step = CurrentStep(job=self)
-
-        # break execution if input is required by the current Step
-        if current_step.requires_input and self.state != JobState.INPUT_RECEIVED:
-            self.state = JobState.INPUT_REQUIRED
-            self.save()
+        try:
+            current_step = self._step_initialize(StepClass)
+        except InputRequired:
             return
 
-        self.state = JobState.ONGOING
-        self.save()
-
-        previous_step_result = (
+        # previous step result
+        _input = (
             self.storage["data"][self.current_step_number - 1]["result"]
             if self.storage["data"]
             else None
         )
 
-        result = current_step._perform_execute(_input=previous_step_result)
+        result = self._step_execute(current_step, _input=_input)
+        transition = self._step_calculate_transition(
+            current_step, result=result, _input=_input
+        )
+
+        try:
+            self._workflow_transition(WorkflowClass, StepClass, transition)
+        except FinishedWorkflow:
+            return
+
+        self._run_next()
+
+    def _step_initialize(self, StepClass: type):
+        """
+        Method initializing currently executed Step instance
+
+        :param StepClass: class object inheriting from django_wfe.steps.Step
+        :raises InputRequired: in case current Step requires an external input (similarly to StopIteration exception)
+        :return: StepClass instance
+        """
+        step = StepClass(job=self)
+
+        # break execution if input is required by the current Step
+        if step.requires_input and self.state != JobState.INPUT_RECEIVED:
+            self.state = JobState.INPUT_REQUIRED
+            self.save()
+
+            self._log(
+                f"Step #{self.current_step_number} '{StepClass.__name__}': input required"
+            )
+
+            raise InputRequired
+        else:
+            self._log(
+                f"Step #{self.current_step_number} '{step.__class__.__name__}': processing started"
+            )
+
+        self.state = JobState.ONGOING
+        self.save()
+
+        return step
+
+    def _step_execute(self, step, _input=None):
+        """
+        Method conducting execute() method of the Step
+
+        :param step: instance of the class inheriting from django_wfe.steps.Step
+        :param _input: previous step's output
+        :return: result of the step execution
+        """
+
+        self._log(
+            f"Step #{self.current_step_number} '{step.__class__.__name__}': performing execute():"
+        )
+
+        try:
+            with Tee(self.logfile, "a"):
+                result = step._perform_execute(_input=_input, logfile=self.logfile)
+
+        except Exception as exception:
+            # log exception in the logfile
+            with open(self.logfile, "a") as log:
+                log.write(
+                    "".join(
+                        traceback.TracebackException.from_exception(exception).format()
+                    )
+                )
+            raise
+
+        self._log(
+            f"Step #{self.current_step_number} '{step.__class__.__name__}': execution finished successfully with a result: {result}"
+        )
 
         try:
             self.storage["data"][self.current_step_number]["result"] = result
@@ -188,40 +283,90 @@ class Job(models.Model):
             )
         self.save()
 
-        transition = current_step._perform_transition(_input=previous_step_result)
+        return result
+
+    def _step_calculate_transition(self, step, _input=None, result=None) -> int:
+        """
+        Method conducting transition() method of the Step
+
+        :param step: instance of the class inheriting from django_wfe.steps.Step
+        :param _input: previous step's output
+        :param result: result of the step execution
+        :return: index of the next step in the Workflow's DIGRAPH
+        """
+        self._log(
+            f"Step #{self.current_step_number} '{step.__class__.__name__}': performing transition():"
+        )
+
+        try:
+            with Tee(self.logfile, "a"):
+                transition = step._perform_transition(_input=_input, result=result)
+
+        except Exception as exception:
+            # log exception in the logfile
+            with open(self.logfile, "a") as log:
+                log.write(
+                    "".join(
+                        traceback.TracebackException.from_exception(exception).format()
+                    )
+                )
+            raise
+
+        self._log(
+            f"Step #{self.current_step_number} '{step.__class__.__name__}': transition finished successfully with a result: {transition}"
+        )
+
+        return transition
+
+    def _workflow_transition(
+        self, WorkflowClass: type, StepClass: type, transition: int = 0
+    ):
+        """
+        Method moving Job's execution to the next node of DIGRAPH
+
+        :param WorkflowClass: class object inheriting from django_wfe.workflows.Workflow
+        :param StepClass: class object inheriting from django_wfe.steps.Step
+        :param transition: index of the next Step in the Workflow's DIGRAPH
+        :raises FinishedWorkflow: in case currently executed Step is the last one in the workflow (similarly to StopIteration exception)
+        :return: None
+        """
 
         if (
-            Workflow.DIGRAPH.get(CurrentStep) is None
-            or len(Workflow.DIGRAPH.get(CurrentStep)) == 0
+            WorkflowClass.DIGRAPH.get(StepClass) is None
+            or len(WorkflowClass.DIGRAPH.get(StepClass)) == 0
         ):
             # workflow's finished
             self.state = JobState.FINISHED
             self.save()
-            return
+
+            self._log(f"---- WORKFLOW FINISHED SUCCESSFULLY ----")
+
+            raise FinishedWorkflow
+
+        self._log(
+            f"Step #{self.current_step_number} '{StepClass.__name__}': step finished"
+        )
 
         self.current_step = Step.objects.get(
             path=(
-                Workflow.DIGRAPH.get(CurrentStep)[transition].__module__
+                WorkflowClass.DIGRAPH.get(StepClass)[transition].__module__
                 + "."
-                + Workflow.DIGRAPH.get(CurrentStep)[transition].__name__
+                + WorkflowClass.DIGRAPH.get(StepClass)[transition].__name__
             )
         )
         self.current_step_number += 1
         self.save()
 
-        self._run_next()
-
-    def _import_class(self, path: str):
+    def _log(self, msg: str):
         """
-        Method importing a certain class from python module.
+        Method logging the message to file and printing it to stdout
 
-        :param path: python path (dot notation) to the class
-        :return: class object under located under the provided path
+        :param msg: string to be logged to file and printed on stdout
+        :return: None
         """
-        module, class_ = path.rsplit(".", 1)
-        Class = getattr(importlib.import_module(module), class_)
 
-        return Class
+        with Tee(self.logfile, "a"):
+            print(f"{datetime.datetime.now()} {msg}")
 
 
 class Watchdog(Singleton):
